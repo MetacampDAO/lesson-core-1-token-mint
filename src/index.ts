8f62a371b56a2802a933b78daeb4d3b6f72ad3fc
Justin Lee <justinleeyile@gmail.com>
2023-09-19 14:06:28 +0800

    update guide

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import { initializeKeypair } from "./initializeKeypair";
 import * as web3 from "@solana/web3.js";
+import * as token from "@solana/spl-token";
 
 const main = async () => {
-  const connection = new web3.Connection(web3.clusterApiUrl("devnet"));
+  const connection = new web3.Connection(web3.clusterApiUrl("testnet"));
   const user = await initializeKeypair(connection);
 
   // Step 1: Create a mint
@@ -10,28 +11,57 @@ const main = async () => {
   // Let's create a createNewMint function in src/utils.ts
   // We can create a mint by using the method createMint: Hint => Refer to the solana-labs spl github docs above
 
+  // Create Mint
+  const tokenMint = await token.createMint(
+    connection,
+    user,
+    user.publicKey,
+    user.publicKey,
+    2
+  );
+
   // Step 2: Find ATA of user
   // Create mint: https://solana-labs.github.io/solana-program-library/token/js/functions/getOrCreateAssociatedTokenAccount.html
   // Create a createTokenAccount function in src/utils.ts
   // We can get or create an ata by using the method getOrCreateAssociatedTokenAccount: Hint => Refer to the solana-labs spl github docs above
+  // Find ATA
+  const tokenAccount = await token.getOrCreateAssociatedTokenAccount(
+    connection,
+    user,
+    tokenMint,
+    user.publicKey
+  );
 
   // Step 3: Mint our token to the user ATA
   // Create mint: https://solana-labs.github.io/solana-program-library/token/js/functions/mintTo.html
   // Create a mintTokens function in src/utils.ts
   // We can issue new token via the mint by using the method mintTo: Hint => Refer to the solana-labs spl github docs above
   // For the mintTo method, you need the mintInfo which you can retrieve via the method getMint
+  const mintInfo = await token.getMint(connection, tokenMint);
 
-  // Now that you have all 3 functions ready, let's put it together
-  // createNewMint
-  // createTokenAccount
-  // mintTokens
+  const transactionSignature = await token.mintTo(
+    connection,
+    user,
+    tokenMint,
+    tokenAccount.address,
+    user.publicKey,
+    100 * 10 ** mintInfo.decimals
+  );
+
+  console.log("SIGNATURE: ", transactionSignature);
 
   // Bonus Challenge!
   // Figure out how to transfer and burn tokens
   // Hint 1: You need to create a new user to transfer to
-  // Hint 2: Use the transfer method and the burn method
+  // Hint 2: The new user need to have a token account
+
+  // Hint 3: Use the transfer method and the burn method
   // Transfer: https://solana-labs.github.io/solana-program-library/token/js/functions/transfer.html
   // Burn: https://solana-labs.github.io/solana-program-library/token/js/functions/burn.html
+
+  // TRANSFER Tx
+
+  // BURN Tx
 };
 
 main()
